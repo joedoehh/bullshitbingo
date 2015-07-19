21f02e9670db2886d7368085dd282b291d722a04
joedoe <doelfer@gmail.com>
2015-07-19 22:17:25 +0200

    initially added webclient

---
+++ bullshitbingo-webclient/app.js
@@ -0,0 +1,86 @@
+'use strict';
+
+angular.module('bullshitBingoApp', ['ngRoute'])
+
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', { templateUrl: 'game.html' })
+      .when('/recorder', { templateUrl: 'recorder.html' })
+      .otherwise({ redirectTo: '/'});
+  })
+     
+  .controller('RecordingsCtrl', function($http){
+	  
+	  var RECORDINGS_REST_SERVICE_BASE_URL = 'http://localhost:9080/bullshitbingo-server/bsb/recording';	  
+	  var recordingsController = this;
+	  
+	  recordingsController.recordings = [];
+	  recordingsController.serverError = 1;
+	  recordingsController.newRecording = "";
+	  
+	  recordingsController.deleteAll = function () {
+		  $http.delete(RECORDINGS_REST_SERVICE_BASE_URL)
+		  .success(
+				  function(data) {
+					  recordingsController.reload();
+				  })
+		  .error(
+				  function(data, status) {
+					  recordingsController.recordings = [];
+					  recordingsController.serverError = 1;
+		    })};	  
+
+	  recordingsController.addRecording = function () {
+		  if (recordingsController.newRecording == "") {
+			  return;
+		  }
+		  var words = recordingsController.newRecording.split(/\b\s+/);
+		  var wordsQueryParam = "";
+		  for (var i = 0; i < words.length; i++) { 
+			  wordsQueryParam += "word="+encodeURIComponent(words[i]);
+			  if (i < words.length-1) {
+				  wordsQueryParam += "&";
+			  }  
+		  }		  
+		  $http.put(RECORDINGS_REST_SERVICE_BASE_URL+"?"+wordsQueryParam)
+		  .success(
+				  function(data) {
+					  recordingsController.reload();
+				  })
+		  .error(
+				  function(data, status) {
+					  recordingsController.recordings = [];
+					  recordingsController.serverError = 1;
+		    })};
+	  
+	  recordingsController.deleteRecording = function (recording) {
+		  $http.delete(RECORDINGS_REST_SERVICE_BASE_URL+"\\"+recording.id)
+		  .success(
+				  function(data) {
+					  recordingsController.reload();
+				  })
+		  .error(
+				  function(data, status) {
+					  recordingsController.recordings = [];
+					  recordingsController.serverError = 1;
+		    })};
+	  
+	    recordingsController.reload = function (recording) {	    
+	    	$http.get(RECORDINGS_REST_SERVICE_BASE_URL)
+	    	.success(
+	    			function(data) {
+	    				recordingsController.recordings = data;
+	    				recordingsController.serverError = 0;
+	    			})
+	    	.error(
+	    			function(data, status) {
+	    				recordingsController.recordings = [];
+	    				recordingsController.serverError = 1;
+	    			})};
+	    
+	    recordingsController.reload();			
+	  	 
+  });
+
+
+  
---
+++ bullshitbingo-webclient/game-data.js
@@ -0,0 +1,14 @@
+{
+		"state":"RUNNING",
+		"lastMatchedRecording":
+				[{"marked":false, "word":"der"},
+				 {"marked":true,"word":"aufstand"},
+				 {"marked":false,"word":"der"},
+				 {"marked":true,"word":"anständigen"}],
+		"games":[{	"player":"joedoe","" +
+					"rating":0,
+					"words":[{"marked":true,"word":"aufstand"},
+					         {"marked":false,"word":"der"},
+					         {"marked":true,"word":"anständigen"},
+					         {"marked":false,"word":"die"},
+					         {"marked":false,"word":"das"}]}]}
